198908906156620191f9e589b3953ed910df8236
michalLewenstein <lmichal2024@gmail.com>
2024-09-29 20:37:49 +0300

    Add files via upload

---
+++ Events/Events/Event.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Event
+    {
+        public int Id;
+        public string Title;
+        public DateTime start;
+
+        private static List<Event> events = new List<Event>() ;
+        private static int currentId=1;
+
+        public Event()
+        {
+
+        }
+        public Event( string Title, DateTime start)
+        {
+            Id = currentId++;
+            this.Title = Title;
+            this.start = start;
+           
+        }
+
+        //כל האירועים
+        public static void AllEvent()
+        {
+            foreach (Event e in events)
+            {
+                Console.WriteLine($" id:{e.Id} ,Title:{e.Title}, start:{e.start}");
+            }
+        }
+        //הוספת אירוע
+        public static void AddEvent(string title, DateTime start) 
+        {
+           Event e= new Event(title,start);
+            events.Add(e);
+        }
+
+        //עדכון כותרת אירוע לפי ID
+
+        public static void UpdateTitle(string title, int id) { 
+            Event e = events.FirstOrDefault(ev => ev.Id==id);
+            if (e!=null)
+            {
+                e.Title = title;
+            }
+        }
+
+        //מחיקת אירוע לפי ID
+
+        public static void DeleteEvent(int id)
+        {
+            Event e = events.FirstOrDefault(ev => ev.Id == id);
+            if (e != null)
+            {
+                events.Remove(e);
+            }
+            else
+            {
+                Console.WriteLine($"Event with ID {id} not found!");
+            }
+        }
+    }
+}
---
+++ Events/Events/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Event.AddEvent("Meeting with Team", DateTime.Now.AddDays(1));
+            Event.AddEvent("Doctor Appointment", DateTime.Now.AddDays(3));
+            Event.AddEvent("Project Deadline", DateTime.Now.AddDays(7));
+            Event.AddEvent("Family Dinner", DateTime.Now.AddDays(5));
+
+            Event.AllEvent();
+
+            Event.AddEvent("llll ", DateTime.Now.AddDays(9));
+
+
+        }
+    }
+}
